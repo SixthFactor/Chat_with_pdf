af7ca8b999ce4eecb8e23692a7626a45cb069c5c
SixthFactorConsulting <deepak.kori@sixthfactor.com>
2024-04-02 15:33:29 +0530

    Update chatpdf.py

--- chatpdf.py
+++ chatpdf.py
@@ -32,7 +32,7 @@ def get_pdf_text(pdf_docs):
 
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=100000, chunk_overlap=10000)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
     chunks = text_splitter.split_text(text)
     return chunks
 
