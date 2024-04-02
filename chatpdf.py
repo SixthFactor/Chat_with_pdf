28a34b67364e1cce761397fed4242c0e6b5f18fa
SixthFactorConsulting <deepak.kori@sixthfactor.com>
2024-04-02 15:34:23 +0530

    Update chatpdf.py

--- chatpdf.py
+++ chatpdf.py
@@ -56,7 +56,7 @@ def get_conversational_chain():
     """
 
     model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.4)
+                             temperature=0.5)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
